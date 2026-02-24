bff3b5e1aec243852b0ea262badbb972f051c2bc
huyaxin1217 <huyaxin1217@gmail.com>
2026-02-24 18:03:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // <--- 就加这一行，记得后面的逗号不能丢
+    base: '-1', // <--- 就加这一行，记得后面的逗号不能丢
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
